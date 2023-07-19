0b502f2912026028a8368466eb64112dd874f231
Duminda Kuruppu <kuruppu.duminda@gmail.com>
2023-07-19 15:54:44 +0530

    moment library added

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/*<Final/>*/}
-        {/*  <Fun/>*/}
+          <Fun/>
         {/*  <MySortableTree/>*/}
-          <LearnCSS/>
+        {/*  <LearnCSS/>*/}
       </header>
     </div>
   );
--- src/Components/Fun.tsx
+++ src/Components/Fun.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import moment from "moment";
 import {render} from "@testing-library/react";
 import Styles from '../Styles'
 import { Form, Field } from "react-final-form";
 import {useState} from "react";
 
+const today = moment.utc(new Date()).format('YYYY MMMM DD (dddd)')
+
+console.log(today)
+
 const Fun = () => {
     return (
-        <h1 style={{flexDirection: "column", alignContent: "center"}}>Fun</h1>
+      <div>
+        <h1 style={{flexDirection: "column", alignContent: "center", fontSize: '2rem'}}>{`${today}`}</h1>
+      </div>
     )
 }
 
